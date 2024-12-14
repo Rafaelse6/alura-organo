8725e9f683c651b897e427efe752c81f3cc8558b
Rafael Santos <rafa.05125@gmail.com>
2024-12-14 16:46:13 -0300

    Name input

---
+++ src/components/CampoTexto/index.jsx
@@ -0,0 +1,11 @@
+import "../CampoTexto/CampoTexto.css";
+function CampoTexto() {
+  return (
+    <div className="campo-texto">
+      <label>Nome</label>
+      <input placeholder="Digite o seu nome"/>
+    </div>
+  );
+}
+
+export default CampoTexto;
