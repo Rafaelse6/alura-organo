55063a2a0e87e1973914a249a17cd29c8ca6f00f
Rafael Santos <rafa.05125@gmail.com>
2024-12-16 12:03:07 -0300

    Updating form

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -5,16 +5,6 @@ import ListaSuspensa from "../ListSuspensa";
 import "./Formulario.css";
 
 const Formulario = (props) => {
-  const times = [
-    "Programação",
-    "Front-End",
-    "Data Science",
-    "Devops",
-    "UX e Design",
-    "Mobile",
-    "Inovação e Gestão",
-  ];
-
   const [nome, setNome] = useState("");
   const [cargo, setCargo] = useState("");
   const [imagem, setImagem] = useState("");
@@ -28,6 +18,10 @@ const Formulario = (props) => {
       imagem,
       time,
     });
+    setNome("");
+    setCargo("");
+    setImagem("");
+    setTime("");
   };
   return (
     <section className="formulario">
@@ -56,7 +50,7 @@ const Formulario = (props) => {
         <ListaSuspensa
           obrigatorio={true}
           label="Time"
-          itens={times}
+          itens={props.times}
           valor={time}
           aoAlterado={(valor) => setTime(valor)}
         />
