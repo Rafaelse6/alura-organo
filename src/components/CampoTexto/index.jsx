9916b9ff4ab32dd7f8097a13076e22ca0013fbd0
Rafael Santos <rafa.05125@gmail.com>
2024-12-16 11:34:29 -0300

    User input, useState and adding collaborator

--- src/components/CampoTexto/index.jsx
+++ src/components/CampoTexto/index.jsx
@@ -3,18 +3,15 @@ import "./CampoTexto.css";
 const CampoTexto = (props) => {
   const placeholderModificada = `${props.placeholder}...`;
 
-  let valor = "Guilherme Silveira";
-
   const aoDigitado = (evento) => {
-    valor = evento.target.value;
-    console.log(valor);
+    props.aoAlterado(evento.target.value);
   };
 
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
       <input
-        value={valor}
+        value={props.valor}
         onChange={aoDigitado}
         required={props.obrigatorio}
         placeholder={placeholderModificada}
